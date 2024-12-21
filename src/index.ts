17924af787d35a8287b92ed342460339bda87737
Velauther Vijayaruban <vvijayarub@pattonair.com>
2024-12-21 01:29:50 +0000

    Reading a json file and displaying in a list

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,25 @@
-interface IHomer {
-    name(): String;
-}
+import { promises as fs } from "fs";
 
+// main async / await
+const main = async () => {
 
-class Homer implements IHomer {
-    name(){
-        return 'Homer Simpson';
+    const filePath = "C:/src/business/utility/my-json-viewer/src/test.json";        
+    const data = await fs.readFile(filePath, "utf-8");
+    const usageData = JSON.parse(data);
+    var months = usageData.months;
+    var monthNames = Object.keys(months);
+    console.log("Month, Total");
+    for (let i = 0; i < monthNames.length; i++) {
+        const monthName = monthNames[i];
+        const monthData = months[monthName];
+        const [month, year] = monthName.split('-'); 
+        const reversedDate = `${year}-${month}-01`; 
+        console.log(`"${reversedDate}","${monthData.total}"`);
     }
-}
+};
 
-const instance = new Homer();
-console.log(instance.name());
+// run main
+// main();
+(async () => { 
+    await main();
+})();
