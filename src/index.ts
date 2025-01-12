146a24c75f073e1601c7cd79ae8f6229ea52d3a9
vvijayarub <vvijayarub@incora.com>
2025-01-12 01:55:50 +0000

    2025-01-12

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,31 @@
-import { promises as fs } from "fs";
+import { promises as fs, PathLike } from "fs";
 
 // main async / await
 const main = async () => {
-
-    const filePath = "C:/src/business/utility/my-json-viewer/src/test.json";        
+    let textFunc = (s : string) => console.log(s);
+    const filePath = "C:/src/Personal/business/utility/my-json-viewer/src/test.json";        
+    // const filePath = "C:/src/Personal/business/utility/months_partitionType_ALL.json";    
+    const outputFile = "C:/src/Personal/business/utility/Units.csv";
     const data = await fs.readFile(filePath, "utf-8");
+    let fileFunc = async (path :PathLike, data :string) => await fs.writeFile(path, data);
     const usageData = JSON.parse(data);
     var months = usageData.months;
     var monthNames = Object.keys(months);
-    console.log("Month, Total");
+    var lines: string[] = [];
+    textFunc = s => lines.push(s);
+    textFunc(`Month, Total`);
     for (let i = 0; i < monthNames.length; i++) {
         const monthName = monthNames[i];
         const monthData = months[monthName];
         const [month, year] = monthName.split('-'); 
         const reversedDate = `${year}-${month}-01`; 
-        console.log(`"${reversedDate}","${monthData.total}"`);
+        textFunc(`${reversedDate},${monthData.total}`);
+        // textFunc(`${reversedDate},${monthData.partitions.ALL.Units}`);
     }
+
+    await fileFunc(outputFile, lines.join('\n'));
+    console.log("Done");
 };
 
 // run main
-// main();
-(async () => { 
-    await main();
-})();
+main();
